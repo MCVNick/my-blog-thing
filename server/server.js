99019f14cb5aa6005afff7e4083a0d0de9fbbff4
MCVNick <mcvnick@gmail.com>
2019-01-24 18:24:11 -0700

    first commit

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+require('dotenv').config()
+
+const app = express()
+app.use(bodyParser.json())
+
+
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, ()=>console.log('listening on port', PORT))
